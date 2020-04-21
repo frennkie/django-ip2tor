6052e198e2bb479404541f7a4bef3850b58d8c4b
Robert Habermann <mail@rhab.de>
2020-04-21 21:24:21 +0200

    add protobuf implementation to parse Macaroon

---
+++ charged/lnnode/protobuf/__init__.py
@@ -0,0 +1,6 @@
+from .id_pb2 import MacaroonId, Op
+
+__all__ = [
+    'MacaroonId',
+    'Op'
+]
--- charged/lnnode/utils.py
+++ charged/lnnode/utils.py
@@ -1,10 +1,13 @@
 import codecs
 
+from protobuf_to_dict import protobuf_to_dict
 from pymacaroons.serializers import BinarySerializer
+
+from charged.lnnode.protobuf import MacaroonId
 
 
 def parse_lnd_macaroon_identifier(macaroon_hex):
-    """Parse the LND specific identifier from a hex encoded macaroon
+    """Parse the LND specific identifier from a hex encoded macaroon using protobuf
 
     Args:
         macaroon_hex (bytes): containing hex byte string
@@ -12,6 +15,29 @@ def parse_lnd_macaroon_identifier(macaroon_hex):
     Examples:
 
     >>> parse_lnd_macaroon_identifier(b'0201036...CF17CE17')
+    {'nonce': b'dead', 'storage_id': b'0', 'ops': [{'entity': 'invoices': 'actions': ['read', 'write']}]}
+
+    Notes:
+        Use dict comprehension to extract ops dict:
+        {item['entity']: item['actions'] for item in protobuf_to_dict(m_id)['ops']}
+
+    """
+    macaroon_bytes = codecs.decode(macaroon_hex, 'hex')
+    macaroon = BinarySerializer().deserialize_raw(macaroon_bytes)
+
+    m_id = MacaroonId().FromString(macaroon.identifier_bytes[1:])
+    return protobuf_to_dict(m_id)
+
+
+def parse_lnd_macaroon_identifier_no_pb(macaroon_hex):
+    """Parse the LND specific identifier from a hex encoded macaroon without protobuf
+
+    Args:
+        macaroon_hex (bytes): containing hex byte string
+
+    Examples:
+
+    >>> parse_lnd_macaroon_identifier_no_pb(b'0201036...CF17CE17')
     {'invoices': ['read', 'write']}
 
     """
