9707d35cb4db67fdc4e8c0b349e1c770b1ab3bd0
Robert Habermann <mail@rhab.de>
2020-04-21 21:55:39 +0200

    add hint / todo to deprecated parser

--- charged/lnnode/utils.py
+++ charged/lnnode/utils.py
@@ -6,6 +6,11 @@ from pymacaroons.serializers import BinarySerializer
 from charged.lnnode.protobuf import MacaroonId
 
 
+class UnsupportedMacaroonIdentifierError(Exception):
+    """The version of the Macaroon identifier is not supported"""
+    pass
+
+
 def parse_lnd_macaroon_identifier(macaroon_hex):
     """Parse the LND specific identifier from a hex encoded macaroon using protobuf
 
@@ -24,15 +29,22 @@ def parse_lnd_macaroon_identifier(macaroon_hex):
         }
     }
 
+    Raises:
+        UnsupportedMacaroonIdentifierError: if version byte does not matched an implemented version
+
     Notes:
         Use dict comprehension to extract ops dict:
         {item['entity']: item['actions'] for item in protobuf_to_dict(m_id)['ops']}
 
     """
+
     macaroon_bytes = codecs.decode(macaroon_hex, 'hex')
     macaroon = BinarySerializer().deserialize_raw(macaroon_bytes)
 
     id_version = int(macaroon.identifier_bytes[0])
+    if not id_version == 3:
+        raise UnsupportedMacaroonIdentifierError
+
     id_bytes = macaroon.identifier_bytes[1:]
 
     m_id = MacaroonId().FromString(id_bytes)
@@ -49,6 +61,11 @@ def parse_lnd_macaroon_identifier_no_pb(macaroon_hex):
 
     >>> parse_lnd_macaroon_identifier_no_pb(b'0201036...CF17CE17')
     {'invoices': ['read', 'write']}
+
+    Notes:
+        This is very hacky..! Don't use it..
+        ToDo(frennkie): remove this
+
 
     """
     macaroon_bytes = codecs.decode(macaroon_hex, 'hex')
