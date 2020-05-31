46301337c7c9fa9cfcecd6b10814129b7fcc9666
Robert Habermann <mail@rhab.de>
2020-05-31 14:28:32 +0200

    rename to IP2Tor

--- scripts/ip2torc.sh
+++ scripts/ip2torc.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# tor2ipc.sh
+# ip2torc.sh
 #
 # License: MIT
 # Copyright (c) 2020 The RaspiBlitz developers
@@ -10,11 +10,11 @@ set -u
 
 # command info
 if [ $# -eq 0 ] || [ "$1" = "-h" ] || [ "$1" = "-help" ] || [ "$1" = "--help" ]; then
-  echo "management script to add, check, list or remove Tor2IP bridges (using socat and systemd)"
-  echo "tor2ipc.sh add [PORT] [TARGET]"
-  echo "tor2ipc.sh check [TARGET]"
-  echo "tor2ipc.sh list"
-  echo "tor2ipc.sh remove [PORT]"
+  echo "management script to add, check, list or remove IP2Tor bridges (using socat and systemd)"
+  echo "ip2torc.sh add [PORT] [TARGET]"
+  echo "ip2torc.sh check [TARGET]"
+  echo "ip2torc.sh list"
+  echo "ip2torc.sh remove [PORT]"
   exit 1
 fi
 
@@ -36,7 +36,7 @@ function add_bridge() {
   target=${2}
   echo "adding bridge from port: ${port} to: ${target}"
 
-  file_path="/etc/systemd/system/tor2ip${port}.service"
+  file_path="/etc/systemd/system/ip2tor${port}.service"
   if [ -f "${file_path}" ]; then
     echo "file exists already"
     # TODO possibly restart..?!
@@ -46,7 +46,7 @@ function add_bridge() {
   # TODO (debian-tor user?! or root?)
   cat <<EOF | sudo tee "${file_path}" >/dev/null
 [Unit]
-Description=Tor2IP Tunnel Service (Port ${port})
+Description=IP2Tor Tunnel Service (Port ${port})
 After=network.target
 
 [Service]
@@ -59,8 +59,8 @@ StandardOutput=journal
 WantedBy=multi-user.target
 EOF
 
-  sudo systemctl enable tor2ip"${port}"
-  sudo systemctl start tor2ip"${port}"
+  sudo systemctl enable ip2tor"${port}"
+  sudo systemctl start ip2tor"${port}"
 
 }
 
@@ -70,18 +70,17 @@ function check_bridge_target() {
   echo "no idea yet what and how to check!"
 }
 
-
 function list_bridges() {
   echo "# Bridges (PORT|TARGET|STATUS)"
   echo "# ============================"
 
-  for f in /etc/systemd/system/tor2ip*.service; do
+  for f in /etc/systemd/system/ip2tor*.service; do
     [ -e "$f" ] || continue
 
     cfg=$(sed -n 's/^ExecStart.*TCP4-LISTEN:\([0-9]*\),.*SOCKS4A:localhost:\(.*\),socksport=.*$/\1|\2/p' "${f}")
     port=$(echo "${cfg}" | cut -d"|" -f1)
     target=$(echo "${cfg}" | cut -d"|" -f2)
-    status=$(systemctl status "tor2ip${port}.service" | grep "Active" | sed 's/^ *Active: //g')
+    status=$(systemctl status "ip2tor${port}.service" | grep "Active" | sed 's/^ *Active: //g')
     echo "${port}|${target}|${status}"
   done
 
@@ -92,22 +91,19 @@ function remove_bridge() {
   port=${1}
   echo "removing bridge from port: ${port}"
 
-  file_path="/etc/systemd/system/tor2ip${port}.service"
+  file_path="/etc/systemd/system/ip2tor${port}.service"
   if ! [ -f "${file_path}" ]; then
     echo "file does not exist"
     echo "no bridge on this port..!"
     exit 1
   fi
 
-  sudo systemctl stop tor2ip"${port}"
-  sudo systemctl disable tor2ip"${port}"
-
-  echo "removing service file.."
+  echo "will now stop, disable and remove service and then refresh systemd"
+  sudo systemctl stop ip2tor"${port}"
+  sudo systemctl disable ip2tor"${port}"
   sudo rm -rf "${file_path}"
-
   sudo systemctl daemon-reload
-  sudo systemctl reset-failed  # TODO(frennkie) not sure whether/why this is needed
-
+  sudo systemctl reset-failed ip2tor"${port}"
   echo "successfully stopped and removed bridge."
 
 }
