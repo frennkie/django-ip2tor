3585a73771cebbd468ea23a7928d0e8d1feb5ec9
Robert Habermann <mail@rhab.de>
2020-07-05 00:00:39 +0200

    migrate process tor bridges mgmt cmd

--- django_ip2tor/settings.py
+++ django_ip2tor/settings.py
@@ -13,9 +13,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 
 # Celery settings
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
-
 CELERY_ACCEPT_CONTENT = ['json']
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
 CELERY_RESULT_BACKEND = 'django-db'
 CELERY_TASK_SERIALIZER = 'json'
 
