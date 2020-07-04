df95452d21c5c540a2e1d820e1aef9e443fee224
Robert Habermann <mail@rhab.de>
2020-07-05 00:01:40 +0200

    migrate process tor bridges mgmt cmd

--- scripts/jobs.sh
+++ scripts/jobs.sh
@@ -23,9 +23,6 @@ do
   /var/www/sites/site_django_ip2tor/venv/bin/python /var/www/sites/site_django_ip2tor/django_ip2tor/manage.py process_lni_unpaid --settings django_ip2tor.settings_prod
   sleep 1
 
-  echo "Job: Process Tor Bridges"
-  /var/www/sites/site_django_ip2tor/venv/bin/python /var/www/sites/site_django_ip2tor/django_ip2tor/manage.py process_tor_bridges --settings django_ip2tor.settings_prod
-
   echo "Sleeping for ${sleep_time} seconds.."
   sleep ${sleep_time}
 
