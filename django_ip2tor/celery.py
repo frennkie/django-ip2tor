2d111a941e8d03f4ce9b284552340f7c62c5edeb
Robert Habermann <mail@rhab.de>
2020-07-05 11:26:24 +0200

    store beat schedule db in /var/run

--- django_ip2tor/celery.py
+++ django_ip2tor/celery.py
@@ -3,7 +3,7 @@ import os
 from celery import Celery
 
 # set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_ip2tor.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_ip2tor.settings_prod')
 
 app = Celery('django_ip2tor')
 
