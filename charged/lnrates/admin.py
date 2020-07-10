28a3a1c408002a52795feeaafa5c4a6f7bf105c8
Robert Habermann <mail@rhab.de>
2020-07-10 23:05:27 +0200

    add aggregate (and clean up) task

--- charged/lnrates/admin.py
+++ charged/lnrates/admin.py
@@ -6,7 +6,7 @@ from charged.lnrates.models import FiatRate, Settings
 @admin.register(FiatRate)
 class ExchangeRateAdmin(admin.ModelAdmin):
     search_fields = ('fiat_symbol', 'coin_symbol', 'source',)
-    list_display = ('fiat_symbol', 'coin_symbol', 'source', 'rate', 'created_at')
+    list_display = ('fiat_symbol', 'coin_symbol', 'source', 'rate', 'is_aggregate', 'created_at')
     list_filter = ('fiat_symbol', 'coin_symbol', 'source', 'is_aggregate', 'created_at')
 
     readonly_fields = ('fiat_symbol', 'coin_symbol', 'source', 'rate', 'is_aggregate', 'fiat_per_coin', 'created_at')
--- charged/lnrates/tasks.py
+++ charged/lnrates/tasks.py
@@ -1,9 +1,13 @@
 import json
+from datetime import timedelta
 
 import certifi
 import urllib3
 from celery import shared_task
 from celery.utils.log import get_task_logger
+from django.db import transaction
+from django.db.models import Avg
+from django.utils import timezone
 
 from charged.lnrates.models import FiatRate
 
@@ -41,5 +45,34 @@ def fetch_rates_for_source_coin_gecko_v1(coin='bitcoin', fiat=None):
     for cur in fiat:
         logger.info(f'{cur[1]}: {result[coin[1]][cur[1]]}')
         FiatRate.objects.create(coin_symbol=coin[0], fiat_symbol=cur[0], rate=result[coin[1]][cur[1]], source=2)
+
+    return True
+
+
+@shared_task()
+def aggregate_rates_for_source_coin_gecko_v1(coin='bitcoin', timedelta_min=60, delay_min=10):
+    if coin == 'bitcoin':
+        coin = (FiatRate.BTC, coin)
+    else:
+        coin = (FiatRate.COIN_UNKNOWN, coin)
+
+    with transaction.atomic():
+        qs = FiatRate.objects \
+            .filter(is_aggregate=False) \
+            .filter(fiat_symbol=FiatRate.EUR) \
+            .filter(created_at__range=(timezone.now() - timedelta(minutes=delay_min) - timedelta(minutes=timedelta_min),
+                                       timezone.now() - timedelta(minutes=delay_min)))
+
+        qs_count = qs.count()
+        rate_avg = qs.aggregate(Avg('rate'))
+        rate = rate_avg['rate__avg']
+
+        logger.info(f'adding aggregated rate for {qs_count} entries: {rate}')
+
+        FiatRate.objects.create(coin_symbol=coin[0], fiat_symbol=FiatRate.EUR,
+                                rate=rate, source=2, is_aggregate=True)
+
+        # remove entries that have now been added as one aggregated value
+        qs.delete()
 
     return True
