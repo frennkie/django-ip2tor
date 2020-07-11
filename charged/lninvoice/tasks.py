e918f2ca7fad202b4eb79636b00b596511652ef6
Robert Habermann <mail@rhab.de>
2020-07-11 16:07:13 +0200

    refactor task

--- charged/lninvoice/tasks.py
+++ charged/lninvoice/tasks.py
@@ -6,30 +6,6 @@ from charged.lninvoice.models import PurchaseOrderInvoice
 logger = get_task_logger(__name__)
 
 
-@shared_task()
-def process_initial_lni(obj_id):
-    logger.info('Running on ID: %s' % obj_id)
-
-    # checks
-    obj: PurchaseOrderInvoice = PurchaseOrderInvoice.objects \
-        .filter(id=obj_id) \
-        .filter(status=PurchaseOrderInvoice.INITIAL) \
-        .first()
-
-    if not obj:
-        logger.info('Not found')
-        return False
-
-    if not obj.lnnode:
-        logger.info('No backend: %s  - skipping' % obj)
-        return False
-
-    obj.lnnode_create_invoice()
-    logger.info(f'New Payment Request: {obj.payment_request}')
-
-    return True
-
-
 class LnInvoiceNoPaymentError(Exception):
     pass
 
@@ -44,6 +20,33 @@ class LnInvoiceNoBackendError(Exception):
 
 class LnInvoiceHasExpiredError(Exception):
     pass
+
+
+@shared_task(bind=True,
+             autoretry_for=(LnInvoiceNoPaymentError,),
+             default_retry_delay=10,
+             retry_kwargs={'max_retries': 2})
+def process_initial_lni(obj_id):
+    logger.info('Running on ID: %s' % obj_id)
+
+    # checks
+    obj: PurchaseOrderInvoice = PurchaseOrderInvoice.objects \
+        .filter(id=obj_id) \
+        .filter(status=PurchaseOrderInvoice.INITIAL) \
+        .first()
+
+    if not obj:
+        logger.info('Not found')
+        raise LnInvoiceNotFoundError()
+
+    if not obj.lnnode:
+        logger.info('No backend: %s  - skipping' % obj)
+        raise LnInvoiceNoBackendError
+
+    obj.lnnode_create_invoice()
+    logger.info(f'New Payment Request: {obj.payment_request}')
+
+    return True
 
 
 @shared_task(bind=True,
