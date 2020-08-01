e4a3aeba5441312c5188bd7c3778d4d758308129
Robert Habermann <mail@rhab.de>
2020-08-01 10:45:27 +0200

    make simpler

--- shop/utils.py
+++ shop/utils.py
@@ -4,13 +4,15 @@ from django.core.mail import EmailMessage
 
 def create_email_message(subject: str, body: str, recipients: list,
                          from_email: str = None,
-                         reference_prefix: str = "ip2tor",
-                         reference_tag: str = "/"):
+                         reference_tag: str = None):
     if from_email is None:
         from_email = getattr(settings, 'DEFAULT_FROM_EMAIL')
 
-    msg = EmailMessage(
-        subject, body, from_email, recipients,
-        headers={'References': f'<{reference_prefix}{reference_tag}/{from_email}>'}
-    )
+    if reference_tag:
+        msg = EmailMessage(
+            subject, body, from_email, recipients,
+            headers={'References': f'<{reference_tag}/{from_email}>'}
+        )
+    else:
+        msg = EmailMessage(subject, body, from_email, recipients)
     return msg
